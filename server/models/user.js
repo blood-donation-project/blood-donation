a8cde1cc6c229136363a34aafc75df833053eeb4
NgVanQuang203 <quangvan14.10.2003@gmail.com>
2024-06-08 09:06:26 +0700

    update db User

--- server/models/user.js
+++ server/models/user.js
@@ -1,82 +1,83 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema(
-    {
-        username: {
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    address: {
+        province: {
             type: String,
             required: true,
         },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        address: {
-            province: {
-                type: String,
-                required: true,
-            },
-            district: {
-                type: String,
-                required: true,
-            },
-            ward: {
-                type: String,
-                required: true,
-            },
-            street: {
-                type: String,
-                required: true,
-            },
-        },
-        phoneNumber: {
-            type: String,
-            unique: true,
-        },
-        dateOfBirth: {
+        district: {
             type: String,
             required: true,
         },
-        gender: {
+        ward: {
             type: String,
             required: true,
         },
-        password: {
+        street: {
             type: String,
             required: true,
-            minlength: 6,
-        },
-        role: {
-            type: String,
-            required: true,
-            default: 'Người hiến máu',
-        },
-        bloodGroup: {
-            type: String,
-            default: '',
-        },
-        avatar: {
-            type: String,
-            default: '',
-        },
-        verified: {
-            type: Boolean,
-            default: false,
-        },
-        block: {
-            type: Boolean,
-            default: false,
-        },
-        introduce: {
-            type: String,
-        },
-        identification: {
-            type: String,
-            default: '',
         },
     },
-    { timestamps: true }
-);
+    phoneNumber: {
+        type: String,
+        unique: true,
+    },
+    dateOfBirth: {
+        type: String,
+        required: true,
+    },
+    gender: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    role: {
+        type: String,
+        required: true,
+        default: 'Người hiến máu',
+    },
+    bloodGroup: {
+        type: String,
+        default: '',
+    },
+    avatar: {
+        type: String,
+        default: '',
+    },
+    introduce: {
+        type: String,
+    },
+    identification: {
+        type: String,
+        default: '',
+    },
+    verified: {
+        type: Boolean,
+        default: false,
+    },
+    block: {
+        type: Boolean,
+        default: false,
+    },
+    createAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
 
 const User = mongoose.model('User', userSchema);
 
