53832b9090c8b13ba2d121a184cb401a8eaaced3
NgVanQuang203 <quangvan14.10.2003@gmail.com>
2024-06-19 22:59:28 +0700

    update fix UserSlice

--- client/src/Redux/features/user/userSlice.js
+++ client/src/Redux/features/user/userSlice.js
@@ -72,25 +72,6 @@ const userSlice = createSlice({
             .addMatcher(userAPI.endpoints.getUserById.matchRejected, (state, action) => {
                 state.error = action.error.message;
             })
-            .addMatcher(userAPI.endpoints.getAllUser.matchFulfilled, (state, { payload }) => {
-                state.user = payload;
-                state.error = null;
-            })
-            .addMatcher(userAPI.endpoints.getAllUser.matchRejected, (state, action) => {
-                state.error = action.error.message;
-                console.log(action);
-            })
-            .addMatcher(userAPI.endpoints.getUserByMonths.matchFulfilled, (state, { payload }) => {
-                state.user = payload;
-                state.error = null;
-            })
-            .addMatcher(userAPI.endpoints.getUserByMonths.matchRejected, (state, action) => {
-                state.error = action.error.message;
-                console.log(action);
-                if (action.payload?.data?.message === 'You are not Admin') {
-                    toast.error('Bạn không có quyền truy cập trang này');
-                }
-            })
             .addMatcher(userAPI.endpoints.lockorUnlockUser.matchFulfilled, (state, { payload }) => {
                 state.user = payload;
                 state.error = null;
