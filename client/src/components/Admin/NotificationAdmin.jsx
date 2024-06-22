352e4bf5acef0ddfb06c46b00089e67cfa4d890d
NgVanQuang203 <quangvan14.10.2003@gmail.com>
2024-06-22 15:29:31 +0700

    update..

--- client/src/components/Admin/NotificationAdmin.jsx
+++ client/src/components/Admin/NotificationAdmin.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Menu from './Menu';
+import { useGetAllNotifiMutation } from '../../Redux/features/notification/notifiAPI';
+import { useGetUserMutation } from '../../Redux/features/user/userAPI';
+import moment from 'moment';
 
 const NotificationAdmin = () => {
+    const [getNotifi, { data: notifiData }] = useGetAllNotifiMutation();
+    const [getUser, { data: userData }] = useGetUserMutation();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                await getUser().unwrap();
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchData();
+    }, [getUser]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                await getNotifi(userData?._id).unwrap();
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchData();
+    }, [getNotifi, userData]);
+
     return (
         <div className="flex h-screen">
             <Menu activeComponent={'notification'} />
@@ -11,24 +39,19 @@ const NotificationAdmin = () => {
                 </div>
                 <div className="p-4 max-w-4xl mx-auto">
                     <div className="bg-white rounded-xl w-full p-4 shadow-sm h-[500px] overflow-y-scroll">
-                        {/* Map here */}
-                        <div className="flex items-center hover:bg-gray-300 p-2 px-3 rounded-md">
-                            <div>
-                                <img
-                                    src=""
-                                    alt=""
-                                />
-                            </div>
-                            <div className="flex flex-col">
-                                <p>
-                                    Đã có hơn 10 bài viết mới từ lần gần đây
-                                    nhất bạn truy cập
-                                </p>
-                                <p className="text-sm text-gray-500">
-                                    2 tuần trước
-                                </p>
-                            </div>
-                        </div>
+                        {notifiData?.length > 0
+                            ? notifiData?.map((item, index) => (
+                                  <div className="flex items-center hover:bg-gray-300 p-2 px-3 rounded-md">
+                                      <div>
+                                          <img src="" alt="" />
+                                      </div>
+                                      <div className="flex flex-col">
+                                          <p dangerouslySetInnerHTML={{ __html: item.content.text }} />
+                                          <p className="text-sm text-gray-500">{moment(item?.createAt).fromNow()}</p>
+                                      </div>
+                                  </div>
+                              ))
+                            : ''}
                     </div>
                 </div>
             </div>
