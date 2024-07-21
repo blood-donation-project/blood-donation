26cf4a877c3599cb95b7b666a8d02deee4974224
NgVanQuang203 <quangvan14.10.2003@gmail.com>
2024-07-21 10:20:28 +0700

    update About

--- client/src/pages/account/About.jsx
+++ client/src/pages/account/About.jsx
@@ -33,19 +33,20 @@ const AboutPage = () => {
                 <div className="max-w-[1150px] mx-auto md:px-4">
                     <div className="bg-white p-4 md:rounded-lg overflow-hidden">
                         <div className="grid md:grid-cols-2 gap-2">
-                            {userData?._id === userCurrentData?._id && (
-                                <div className=" flex  flex-col px-2 xs:py-1.5 md:py-0 xs:border-b xs:border-b-[#ccc] ">
-                                    <div className="flex md:flex-col xs:flex-row">
-                                        <div className="xs:w-[30px] md:w-0 text-[22px] text-[#65676B]">
-                                            <MdEmail />
+                            {userData?._id === userCurrentData?._id ||
+                                userData?.role === 'Cơ sở y tế' ? (
+                                    <div className=" flex  flex-col px-2 xs:py-1.5 md:py-0 xs:border-b xs:border-b-[#ccc] ">
+                                        <div className="flex md:flex-col xs:flex-row">
+                                            <div className="xs:w-[30px] md:w-0 text-[22px] text-[#65676B]">
+                                                <MdEmail />
+                                            </div>
+                                            <div className="text-[#65676B] text-[16px]">Email</div>
                                         </div>
-                                        <div className="text-[#65676B] text-[16px]">Email</div>
+                                        <div className="text-[16px] font-bold text-[#3e3e3e] xs:ml-[30px] md:ml-0  ">
+                                            <span className=" word-wrap">{userData?.email}</span>
+                                        </div>
                                     </div>
-                                    <div className="text-[16px] font-bold text-[#3e3e3e] xs:ml-[30px] md:ml-0  ">
-                                        <span className=" word-wrap">{userData?.email}</span>
-                                    </div>
-                                </div>
-                            )}
+                                ): ''}
 
                             {userData?.role === 'Cơ sở y tế' ? (
                                 ''
@@ -95,31 +96,36 @@ const AboutPage = () => {
                                         : '') + userData?.address.province}
                                 </div>
                             </div>
-                            {userData?._id === userCurrentData?._id && (
+                            {userData?._id === userCurrentData?._id ||
+                                userData?.role === 'Cơ sở y tế' ? (
+                                    <div className=" flex  flex-col px-2 xs:py-1.5 md:py-0 xs:border-b xs:border-b-[#ccc] ">
+                                        <div className="flex md:flex-col xs:flex-row">
+                                            <div className="xs:w-[30px] md:w-0 text-[22px] text-[#65676B]">
+                                                <FaPhone />
+                                            </div>
+                                            <div className="text-[#65676B] text-[16px]">Liên hệ</div>
+                                        </div>
+                                        <div className="text-[16px] font-bold text-[#3e3e3e] xs:ml-[30px] md:ml-0 line-clamp-3">
+                                            {userData?.phoneNumber}
+                                        </div>
+                                    </div>
+                                ): ''}
+
+                            {userData?.role === 'Cơ sở y tế' ? (
+                                ''
+                            ) : (
                                 <div className=" flex  flex-col px-2 xs:py-1.5 md:py-0 xs:border-b xs:border-b-[#ccc] ">
                                     <div className="flex md:flex-col xs:flex-row">
                                         <div className="xs:w-[30px] md:w-0 text-[22px] text-[#65676B]">
-                                            <FaPhone />
+                                            <MdBloodtype />
                                         </div>
-                                        <div className="text-[#65676B] text-[16px]">Liên hệ</div>
+                                        <div className="text-[#65676B] text-[16px]">Nhóm máu</div>
                                     </div>
-                                    <div className="text-[16px] font-bold text-[#3e3e3e] xs:ml-[30px] md:ml-0 line-clamp-3">
-                                        {userData?.phoneNumber}
+                                    <div className="text-[16px] font-bold text-[#3e3e3e] xs:ml-[30px] md:ml-0  ">
+                                        <span className=" word-wrap">{userData?.bloodGroup || 'Không xác định'}</span>
                                     </div>
                                 </div>
                             )}
-
-                            <div className=" flex  flex-col px-2 xs:py-1.5 md:py-0 xs:border-b xs:border-b-[#ccc] ">
-                                <div className="flex md:flex-col xs:flex-row">
-                                    <div className="xs:w-[30px] md:w-0 text-[22px] text-[#65676B]">
-                                        <MdBloodtype />
-                                    </div>
-                                    <div className="text-[#65676B] text-[16px]">Nhóm máu</div>
-                                </div>
-                                <div className="text-[16px] font-bold text-[#3e3e3e] xs:ml-[30px] md:ml-0  ">
-                                    <span className=" word-wrap">{userData?.bloodGroup || 'Không xác định'}</span>
-                                </div>
-                            </div>
                             {userData?._id === userCurrentData?._id && (
                                 <div className=" flex  flex-col px-2 xs:py-1.5 md:py-0 xs:border-b xs:border-b-[#ccc] ">
                                     <div className="flex md:flex-col xs:flex-row">
